d35967564154c56f3b64884f5fdbad25c5b83356
Ruiyot Abby <ruiyotabby@gmail.com>
2022-07-02 14:42:41 +0300

    commit
    Signed-off-by: Ruiyot Abby <ruiyotabby@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
